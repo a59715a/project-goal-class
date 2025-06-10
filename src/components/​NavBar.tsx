7ea3a0960c9c5378c213f92eab83d5b8f86fd051
TCUST_DDian <a59715a@yahoo.com.tw>
2025-06-10 19:42:36 +0800

    init

---
+++ src/components/​NavBar.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { Menubar } from "primereact/menubar";
+import { Button } from "primereact/button";
+import { useRouter } from "next/navigation";
+
+export default function NavBar() {
+  const router = useRouter();
+  const items = [
+    {
+      label: "首頁 Home",
+      icon: "pi pi-home",
+      command: () => router.push("/"),
+    },
+    {
+      label: "註冊 Register",
+      icon: "pi pi-user",
+      command: () => router.push("/register"),
+    },
+    {
+      label: "登入 Login",
+      icon: "pi pi-user",
+      command: () => router.push("/login"),
+    },
+    {
+      label: "會員管理 Member Management",
+      icon: "pi pi-user",
+      command: () => router.push("/members"),
+    },
+    {
+        label: "購物車 Shopping Cart",
+        icon: "pi pi-shopping-cart",
+        command: () => router.push("/cart"),
+    },
+
+
+
+  ];
+
+  const end = (
+    <div className="flex align-items-center gap-2">
+      <Button icon="pi pi-user" rounded text />
+    </div>
+  );
+
+  return (
+    <div className="card">
+      <Menubar model={items} end={end} />
+    </div>
+  );
+}
